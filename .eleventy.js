d61922681bb636cd47584ec68739ca679f5a1509
Joe Hart <joe@joehart.dev>
2023-06-14 10:01:38 +0100

    feat: :sparkles: Only show non-draft posts in final site

--- .eleventy.js
+++ .eleventy.js
@@ -33,6 +33,9 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("dd LLL yyyy");
   });
 
+  eleventyConfig.addFilter("activePostsOnly", postList => {
+    return postList.filter(post => post.data.draft !== true);
+  });
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
